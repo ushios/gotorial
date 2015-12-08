e05847b0ea7e935163c664d3947bb8e14760361f
UshioShugo <ushio.s@gmail.com>
2015-12-08 10:39:21 +0900

    add three add.

--- calc/add.go
+++ calc/add.go
@@ -1,6 +1,13 @@
+// Package calc is calucrator.
 package calc
 
+// Add is addition two ints.
 func Add(a int, b int) (sum int) {
-	sum = a + b
+	sum = threeAdd(a, b, 0)
+	return
+}
+
+func threeAdd(a int, b int, c int) (sum int) {
+	sum = a + b + c
 	return
 }
--- calc/add_test.go
+++ calc/add_test.go
@@ -13,3 +13,16 @@ func TestAdd(t *testing.T) {
 		t.Errorf("sum want (%d)", 25)
 	}
 }
+
+func TestThreeAdd(t *testing.T) {
+	var a, b, c int
+	a = 10
+	b = 20
+	c = 30
+
+	sum := threeAdd(a, b, c)
+
+	if sum != 60 {
+		t.Errorf("sum want (%d)", 60)
+	}
+}
